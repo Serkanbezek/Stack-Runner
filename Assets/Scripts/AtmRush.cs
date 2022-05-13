158f0d66da9f90d1a69a694d98f9f61e5174f618
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-13 17:30:23 +0300

    Update AtmRush.cs

--- Assets/Scripts/AtmRush.cs
+++ Assets/Scripts/AtmRush.cs
@@ -7,10 +7,8 @@ public class AtmRush : MonoBehaviour
 {
     public static AtmRush Instance;
     public float MovementDelay = 0.25f;
-    
-
     public List<GameObject> Collectables = new List<GameObject>();
-
+    
     private void Awake()
     {
         Instance = this;
@@ -18,25 +16,16 @@ public class AtmRush : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButton("Fire1"))
-        {
-            MoveListElements();
-        }
-        if (Input.GetButtonUp("Fire1"))
-        {
-            MoveOrigin();
-        }
+        LerpListElements();
     }
-
-
+    
     public void StackCollectable(GameObject other, int index)
     {
         other.transform.parent = transform;
         Vector3 newPos = Collectables[index].transform.localPosition;
-        newPos.z += 1.2f;
+        newPos.z += 1.1f;
         other.transform.localPosition = newPos;
         Collectables.Add(other);
-       
         StartCoroutine(MakeObjectsBigger());
     }
 
@@ -47,7 +36,6 @@ public class AtmRush : MonoBehaviour
             int index = i;
             Vector3 scale = new Vector3(1, 1, 1);
             scale *= 1.5f;
-
             Collectables[index].transform.DOScale(scale, 0.1f).OnComplete(() =>
             Collectables[index].transform.DOScale(new Vector3(1, 1, 1), 0.1f));
             yield return new WaitForSeconds(0.05f);
@@ -55,24 +43,14 @@ public class AtmRush : MonoBehaviour
         }
     }
 
-    private void MoveListElements()
+    private void LerpListElements()
     {
+        float lerpValue = 20;
         for (int i = 1; i < Collectables.Count; i++)
         {
             Vector3 pos = Collectables[i].transform.localPosition;
-            pos.x = Collectables[i - 1].transform.localPosition.x;
-            Collectables[i].transform.DOLocalMove(pos, MovementDelay);
+            Vector3 finalPos = Collectables[i - 1].transform.localPosition;
+            Collectables[i].transform.localPosition = new Vector3(Mathf.Lerp(pos.x, finalPos.x, Time.deltaTime * lerpValue), pos.y, pos.z);
         }
     }
-
-    private void MoveOrigin()
-    {
-        for (int i = 1; i < Collectables.Count; i++)
-        {
-            Vector3 pos = Collectables[i].transform.localPosition;
-            pos.x = Collectables[0].transform.localPosition.x;
-            Collectables[i].transform.DOLocalMove(pos, 0.70f);
-        }
-    }
-
 }
