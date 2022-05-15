fcd93b27a38c24f3a50027c1b30d6078e3e6d87a
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-16 02:11:28 +0300

    Create ObstacleManager.cs

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            AtmRush.Instance.DestroyItem(other.gameObject);
+        }
+    }
+}
