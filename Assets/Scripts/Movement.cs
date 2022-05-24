52e73949d69435be1d066f42e11155ada76d52a1
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-24 23:27:29 +0300

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,43 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Movement : Singleton<Movement>
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float swipeSpeed;
-    private Vector3 firstPos, endPos;
-    private float offset = 100;
-    private float leftBound = -6.1f;
-    private float rightBound = 2.81f;
-  
-    // Update is called once per frame
+    //private Vector3 firstPos, endPos;
+    private float leftBound = -4.50f;
+    private float rightBound = 1.20f;
+    private float differenceX;
+    private float lastFrameFingerPosX;
+    public GameObject firstCollectable;
+
+    private void Awake() => firstCollectable = AtmRush.Instance.Collectables[0];
+    
     void Update()
     {
-        transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
-        HandleInput();
-        ConstrainPlayer();
+        if (AnimationManager.Instance.isGameActive == true)
+        {
+            ForwardMovement();
+            HandleInput();
+            ConstrainPlayer();
+        }
     }
 
     private void HandleInput()
     {
-        GameObject firstCollectable = AtmRush.Instance.Collectables[0];
+        
         if (Input.GetMouseButtonDown(0))
         {
-            firstPos = Input.mousePosition;
+            lastFrameFingerPosX = Input.mousePosition.x;
         }
         else if (Input.GetMouseButton(0))
         {
-            endPos = Input.mousePosition;
-
-            float differenceX = (endPos.x - firstPos.x);
-            firstCollectable.transform.Translate(differenceX * Time.deltaTime * swipeSpeed / offset, 0, 0);
+            differenceX = Input.mousePosition.x - lastFrameFingerPosX;
+            lastFrameFingerPosX = Input.mousePosition.x;
+            
         }
-        if (Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonUp(0))
         {
-            firstPos = Vector3.zero;
-            endPos = Vector3.zero;
+            differenceX = 0f;
         }
+        firstCollectable.transform.Translate(differenceX * Time.deltaTime * swipeSpeed, 0, 0);
     }
+
+    private void ForwardMovement() => transform.position += moveSpeed * Time.deltaTime * Vector3.forward;
+
     private void ConstrainPlayer()
     {
         GameObject firstCollectable = AtmRush.Instance.Collectables[0];
