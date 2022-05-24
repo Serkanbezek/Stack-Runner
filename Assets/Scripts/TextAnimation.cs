7b3e222b21e26523bac296ef43078bca24b86dc3
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-24 23:27:51 +0300

    Create TextAnimation.cs

---
+++ Assets/Scripts/TextAnimation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TextAnimation : MonoBehaviour
+{
+    [SerializeField] private Vector3 scaleRate = new Vector3(1.2f, 1.2f, 1.2f);
+    [SerializeField] private float scaleDuration = 0.8f;
+
+    private void Start() => transform.DOScale(scaleRate, scaleDuration).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+
+    private void Update()
+    {
+        if (AnimationManager.Instance.isGameActive) gameObject.SetActive(false);
+    }
+}
