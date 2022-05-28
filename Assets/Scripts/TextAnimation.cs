ef1d8a1196b268b475f583f509d5a114cd3aaebc
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-28 13:40:45 +0300

    Update TextAnimation.cs

--- Assets/Scripts/TextAnimation.cs
+++ Assets/Scripts/TextAnimation.cs
@@ -7,11 +7,21 @@ public class TextAnimation : MonoBehaviour
 {
     [SerializeField] private Vector3 scaleRate = new Vector3(1.2f, 1.2f, 1.2f);
     [SerializeField] private float scaleDuration = 0.8f;
+    private bool isKilled = false;
 
-    private void Start() => transform.DOScale(scaleRate, scaleDuration).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+    private void Start()
+    {
+         transform.DOScale(scaleRate, scaleDuration).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+    }
+        
 
     private void Update()
     {
-        if (AnimationManager.Instance.isGameActive) gameObject.SetActive(false);
+        if (GameManager.Instance.isGameActive && isKilled == false)
+        {
+            DOTween.Kill(gameObject.transform);
+            isKilled = true;
+            Destroy(gameObject);
+        }
     }
 }
