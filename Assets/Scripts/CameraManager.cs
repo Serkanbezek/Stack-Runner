522f6201d6b0de7bdb48ae7ece76c76e9f149565
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-28 13:40:06 +0300

    Update CameraManager.cs

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float SmoothTime = 0.3f;
     private Transform playerPos;
     private Transform AllCharactersPos;
     private float offSetX = 1.65f;
     private float offSetZ = 7.9f;
+    private Vector3 camVelocity = Vector3.zero;
+    
 
     private void Awake()
     {
@@ -16,5 +19,5 @@ public class CameraManager : MonoBehaviour
         AllCharactersPos = Movement.Instance.transform;
     }
 
-    void LateUpdate() => transform.position = new Vector3(playerPos.localPosition.x + offSetX, transform.position.y, AllCharactersPos.position.z - offSetZ);
+    void LateUpdate() => transform.position = Vector3.SmoothDamp(transform.position, new Vector3(playerPos.localPosition.x + offSetX, transform.position.y, AllCharactersPos.position.z - offSetZ), ref camVelocity, SmoothTime);
 }
