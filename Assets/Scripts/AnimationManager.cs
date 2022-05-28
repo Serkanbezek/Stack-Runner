713cfc103fca03102ad68365eabf622c87ec40d5
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-28 13:39:59 +0300

    Update AnimationManager.cs

--- Assets/Scripts/AnimationManager.cs
+++ Assets/Scripts/AnimationManager.cs
@@ -2,24 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationManager : Singleton<AnimationManager>
+public class AnimationManager : MonoBehaviour
 {
-    public bool isGameActive;
     private Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        isGameActive = false;
     }
     void Update()
     {
-        if (!isGameActive)
+        if (!GameManager.Instance.isGameActive)
         {
             if (Input.GetMouseButtonUp(0))
             {
-                isGameActive = true;
-                animator.Play("Running");
+                GameManager.Instance.isGameActive = true;
+                animator.SetBool("isRunning", true);
             }
         }
     }
