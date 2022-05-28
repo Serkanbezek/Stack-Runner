7845f186e6f0dd17b72b6a704ccf1a7d35c1ffb3
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-28 13:40:17 +0300

    Create GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    public bool isGameActive = false;
+    public bool isLevelFinished = false;
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
