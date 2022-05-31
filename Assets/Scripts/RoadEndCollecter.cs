6a666bcd092d1a27caeca8fbc033abf925f65705
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-31 03:23:13 +0300

    Update RoadEndCollecter.cs

--- Assets/Scripts/RoadEndCollecter.cs
+++ Assets/Scripts/RoadEndCollecter.cs
@@ -6,6 +6,11 @@ public class RoadEndCollecter : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Collectable") Destroy(other.gameObject);
+        if (other.gameObject.tag == "Collectable")
+        {
+            GameManager.Instance.UpdateScore(other.GetComponent<MPBController>().ValueOfCollectable);
+            Destroy(other.gameObject);
+        }
+        
     }
 }
