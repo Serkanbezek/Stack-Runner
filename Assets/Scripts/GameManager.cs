dbb13218729f7058bcc5fc6832ccd4254d508986
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-05-31 03:22:59 +0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : Singleton<GameManager>
 {
-    public bool isGameActive = false;
-    public bool isLevelFinished = false;
+    public bool IsGameActive = false;
+    public bool IsLevelFinished = false;
+    public TextMeshProUGUI ScoreText;
+    public int Score;
 
+    private void Start()
+    {
+        Score = 0;
+    }
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -16,6 +23,13 @@ public class GameManager : Singleton<GameManager>
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        Score += scoreToAdd;
+        ScoreText.text = "Score: " + Score;
     }
 
 }
